b64cf91bf8ada0035c30a6b5f92e3e0d60445ef1
saltchicken <john.eicher89@gmail.com>
2022-05-30 14:33:03 -0700

    Fixing typo in multi line string

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.create_tests()
 	file = io.open("test_.py", "w")
-	file:write("""
+	file:write([[
 	def func(x):
 		return x + 1
 
 	def test_answer():
 		assert func(3) == 5
-	""")
+		]])
 	file:close()
 end
 return M
