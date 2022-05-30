2a9a9b2904b26868d7a5ce95cbb74b12a168418f
saltchicken <john.eicher89@gmail.com>
2022-05-30 14:35:39 -0700

    Fixing tab issue for CreateTests

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.create_tests()
 	file = io.open("test_.py", "w")
 	file:write([[
-	def func(x):
-		return x + 1
+def func(x):
+	return x + 1
 
-	def test_answer():
-		assert func(3) == 5
+def test_answer():
+	assert func(3) == 5
 		]])
 	file:close()
 end
