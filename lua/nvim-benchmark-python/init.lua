d747f7e8cf2c110ec624fb2b61c12f89fbaa9f66
saltchicken <john.eicher89@gmail.com>
2022-05-30 17:22:53 -0700

    Adding proper filename creation

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -1,7 +1,14 @@
 local M = {}
 
+function get_file_name(file)
+	return file:match("^.+/(.+)$")
+end
+
 function M.create_tests()
-	file = io.open("test_.py", "w")
+	local file_name = vim.fn.expand('%')
+	-- local file_name = get_file_name(vim.fn.expand('%'))
+	print(file_name)
+	file = io.open("test_" .. file_name, "w")
 	file:write([[
 def func(x):
 	return x + 1
@@ -10,5 +17,7 @@ def test_answer():
 	assert func(3) == 5
 		]])
 	file:close()
+	print(vim.api.nvim_buf_get_name(0))
 end
+
 return M
