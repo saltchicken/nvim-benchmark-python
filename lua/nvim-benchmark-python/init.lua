2e822913f3cd5b29fa9233f8ccc1cfc469abe72d
saltchicken <john.eicher89@gmail.com>
2022-05-30 18:02:16 -0700

    CreateTests now splits the screen

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -7,17 +7,18 @@ end
 function M.create_tests()
 	local file_name = vim.fn.expand('%')
 	-- local file_name = get_file_name(vim.fn.expand('%'))
-	print(file_name)
+	-- print(file_name)
 	file = io.open("test_" .. file_name, "w")
 	file:write([[
 def func(x):
 	return x + 1
 
 def test_answer():
-	assert func(3) == 4
+	assert func(3) == 5
 		]])
 	file:close()
-	print(vim.api.nvim_buf_get_name(0))
+	-- print(vim.api.nvim_buf_get_name(0))
+	vim.api.nvim_command(":vsp test_" .. file_name)
 end
 
 return M
