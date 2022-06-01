e9bee9e6d912e2be5272cacf596f03a5223d3773
saltchicken <john.eicher89@gmail.com>
2022-05-31 21:55:16 -0700

    Proper lua matching for parsing out file name

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -6,26 +6,32 @@ end
 
 function M.create_tests()
 	
-	local test_body = ''
-
+	-- Get current file name, path, and import name
+	local file_path = vim.api.nvim_buf_get_name(0)
+	local file_name = get_file_name(file_path)
+	local import_name = file_name:sub(1, -4)
+		
+	-- Iterate through lines to find function names. Assign to funcs. 
 	lines = vim.api.nvim_buf_get_lines(1, 0, -1, true)
 	funcs = {}
 	for i, j in ipairs(lines) do
 		if string.find(j, "def") then
-			table.insert(funcs, j)
+			local func = string.match(j, ".+%(")
+			table.insert(funcs, func:sub(5, -2) )
 		end
 	end
+	
+	local test_body = string.format('import %s', import_name) .. "\n\n"
+
+	-- Iterate through function names and create test functions. Append to test_body.
 	for i, j in ipairs(funcs) do
 		local test_function = [[
-def test_%s:
-	assert func(3) == 5
+def test_%s():
+	assert %s.%s() == True
 		]]
-		test_body = test_body .. string.format(test_function, j) .. "\n"
+		test_body = test_body .. string.format(test_function, j, import_name, j) .. "\n"
 	end
 	
-	local file_name = vim.fn.expand('%')
-	-- local file_name = get_file_name(vim.fn.expand('%'))
-	-- print(vim.api.nvim_buf_get_name(0))
 	
 	file = io.open("test_" .. file_name, "w")
 	file:write(test_body)
