1904b16fe1d65bcfec82a669fd6b6956fec92acc
saltchicken <john.eicher89@gmail.com>
2022-06-01 12:42:32 -0700

    Param parsing function and passing through vim command.

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -4,20 +4,51 @@ function get_file_name(file)
 	return file:match("^.+/(.+)$")
 end
 
-function M.create_tests()
+function get_parameters(func_line)
+	local params = string.match(func_line, "%(.+%)")
+	if params == nil then
+		return nil
+	end
+	params = params:sub(2, -2)
+	local commas = {}
+	local comma_index = 0
+	local param_count = 1
+	while true do
+		comma_index = string.find(params, ",", comma_index + 1)
+		if comma_index == nil then break end
+		table.insert(commas, comma_index)
+		param_count = param_count + 1
+	end
+	-- print(unpack(commas))
+	-- print(param_count)
+	return param_count, params
+
+end
+
+function M.create_tests(test_params, test_results)
 	
-	-- Get current file name, path, and import name
+	-- print(test_params, test_results)
+
+	-- Get current file name, path, and import name.
 	local file_path = vim.api.nvim_buf_get_name(0)
 	local file_name = get_file_name(file_path)
 	local import_name = file_name:sub(1, -4)
-		
+
+	-- Get current cursor position.
+	local cursor_row, cursor_column = unpack(vim.api.nvim_win_get_cursor(0))
+	-- print(cursor_row, cursor_column)
+
 	-- Iterate through lines to find function names. Assign to funcs. 
 	lines = vim.api.nvim_buf_get_lines(1, 0, -1, true)
 	funcs = {}
 	for i, j in ipairs(lines) do
 		if string.find(j, "def") then
 			local func = string.match(j, ".+%(")
-			table.insert(funcs, func:sub(5, -2) )
+			func = func:gsub("%s+", "")
+			table.insert(funcs, func:sub(4, -2) )
+			
+			local param_count, params = get_parameters(j)
+			-- print(param_count, params)
 		end
 	end
 	
--- plugin/nvim-benchmark-python.vim
+++ plugin/nvim-benchmark-python.vim
@@ -6,4 +6,5 @@ let g:loaded_nvimbenchmarkpython = 1
 let s:lua_rocks_deps_loc =  expand("<sfile>:h:r") . "/../lua/nvim-benchmark-python/deps"
 exe "lua package.path = package.path .. ';" . s:lua_rocks_deps_loc . "/lua-?/init.lua'"
 
-command! -nargs=0 CreateTests lua require("nvim-benchmark-python").create_tests()
+"command! -nargs=0 CreateTests lua require("nvim-benchmark-python").create_tests()
+command! -nargs=* CreateTests lua require("nvim-benchmark-python").create_tests(<f-args>)
