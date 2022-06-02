24049560fcc5b2934234712bbea9789f113c5c59
saltchicken <john.eicher89@gmail.com>
2022-06-02 14:49:15 -0700

    Test functions load parameters and expected results

--- lua/nvim-benchmark-python/init.lua
+++ lua/nvim-benchmark-python/init.lua
@@ -2,42 +2,29 @@ require("helper")
 
 local M = {}
 
-function M.create_tests(test_params, test_results)
+function M.create_tests(test_results, test_params)
 	
-	-- print(test_params, test_results)
-
-	-- Get current file name, path, and import name.
 	local file_path = vim.api.nvim_buf_get_name(0)
 	local file_name = get_file_name(file_path)
 	local import_name = file_name:sub(1, -4)
 
-	-- Get current cursor position.
-	-- print(cursor_row, cursor_column)
+	if test_params == nil then test_params = "" end
 
-	-- Iterate through lines to find function names. Assign to funcs. 
-	lines = vim.api.nvim_buf_get_lines(1, 0, -1, true)
-	funcs = {}
-	for i, j in ipairs(lines) do
-		if string.find(j, "def") then
-			local func = string.match(j, ".+%(")
-			func = func:gsub("%s+", "")
-			table.insert(funcs, func:sub(4, -2) )
-			
-			local param_count, params = get_parameters(j)
-			-- print(param_count, params)
-		end
+	if file_exists("test_" .. file_name) then
+		file = io.open("test_" .. file_name, "r")
+		test_body = file:read("*all")
+		file:close()
+	else
+		test_body = string.format('import %s', import_name) .. "\n\n"
 	end
-	local test_body = string.format('import %s', import_name) .. "\n\n"
 
-	-- Iterate through function names and create test functions. Append to test_body.
-	for i, j in ipairs(funcs) do
-		local test_function = [[
-def test_%s():
-	assert %s.%s() == True
-		]]
-		test_body = test_body .. string.format(test_function, j, import_name, j) .. "\n"
+	local test_function = "def test_%s():\n\tassert %s.%s(%s) == %s"
+	local function_name = get_function()
+	if function_name ~= nil then
+		test_body = test_body .. string.format(test_function, function_name, import_name, function_name, test_params, test_results) .. "\n"
+	else
+		print("Function not selected")
 	end
-	
 	
 	file = io.open("test_" .. file_name, "w")
 	file:write(test_body)
